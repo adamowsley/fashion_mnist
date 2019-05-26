595ff77b1b38cf617fd3f120efeaebc6aa800d34
adamowsley <adam.owsley@gmail.com>
2019-05-26 07:54:20 -0400

    Spring cleaning

--- train/fashion_train.py
+++ train/fashion_train.py
@@ -70,15 +70,17 @@ model.add(keras.layers.Dense(10, activation='softmax'))
 model.summary()
 
 # Compile the model
-# The optimizer is not named after me, but I do appreciate it. It is a stochastic optimizer that
-# is efficient and requires little memory. See the Keras documentation for further information.
+# The optimizer is sadly not named after me. It stands for Adaptive Moment Estimation and is a 
+# stochastic optimizer that is efficient and requires little memory. See the Keras documentation for 
+# further information.
 # sparse_categorical_crossentropy loss accepts values between 0 and 1
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
 # Train using the images and their corresponding labels
-train_model = model.fit(train_images, train_labels, epochs=1)
+# epochs lets us select how many training runs we'll use
+train_model = model.fit(train_images, train_labels, epochs=3)
 
 # Evaluate the model using the test images and corresponding labels
 test_loss, test_acc = model.evaluate(test_images, test_labels)
@@ -86,45 +88,7 @@ test_loss, test_acc = model.evaluate(test_images, test_labels)
 print("Test loss: {0:3.2f}%".format(test_loss*100))
 print("Test accuracy: {0:3.2f}%".format(test_acc*100))
 
-# Use test_image[0]; this is suppose to be an ankle boot
-img = test_images[1]
-
-# Remembering the shape of our training and test image, use NumPy to insert the image 
-# into the zeroith index for use in the predicting this one image
-img = (np.expand_dims(img, 0))
-
-# Use the trained model to classify our image
-predictions_single = model.predict(img)
-
-# Convert scientific notation to float. This only affects the display of the numbers
-np.set_printoptions(formatter={'float_kind':'{:f}'.format})
-
-# Multiply each array item by 100
-predictions_single = predictions_single * 100
-
-# Note that the item at index = 9 is very close to 1.0 and the other array values are not
-print(predictions_single)
-
-# These are the names of each of our ten (10) classes of clothing
-class_names = ['Shirt/Top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-
-# Set up matplotlib to produce a plot of our prediction
-y_pos = np.arange(len(class_names))
-
-# predictions_single is currently an array within an array. We flatten it to remove the outer array
-predictions_single = predictions_single.flatten()
-
-plt.rcdefaults()
-plt.bar(y_pos, predictions_single, align='center', alpha=0.5)
-plt.xticks(y_pos, class_names, rotation=45)
-plt.ylabel('Probability')
-plt.title('Image Classification')
-
-#x = np.linspace(0, 1, 100)
-
-# Save a png of our prediction to disk; check your current working directory
-plt.savefig("probability")
-
-# Save the trained model for use in another application
+# Save the trained model for use in another application using the HDF5 format
 model.save("trained_model.h5")
+
+print("Save trained model to trained_model.h5")
